b16b690e6fcb252e877195e2eaea394dd7eec2a1
mjoes <morten.slingsby@gmail.com>
2024-09-21 23:08:30 +0200

    Add custom metadata option

--- cmd/upload/metadata.go
+++ cmd/upload/metadata.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
-  "fmt"
 
 	_ "github.com/mattn/go-sqlite3"
+	"google.golang.org/api/sheets/v4"
 )
 
 type Metadata struct {
@@ -22,12 +23,57 @@ type Metadata struct {
 	tags4       string
 }
 
-func get_metadata(sqlDB *sql.DB, tag string) Metadata {
+func check_custom_metadata(sheet *sheets.ValueRange, metadata Metadata, date string, tag string) (bool, Metadata) {
+	for _, row := range sheet.Values {
+		if row[0] == date && row[1] == tag {
+			for len(row) < 11 {
+				row = append(row, "")
+			}
+			if row[2] != "" {
+				metadata.show_name = row[2].(string)
+			}
+			if row[3] != "" {
+				metadata.dj_name = row[3].(string)
+			}
+			if row[4] != "" {
+				metadata.picture = row[4].(string)
+			}
+			if row[5] != "" {
+				metadata.description = row[5].(string)
+			}
+			if row[6] != "" {
+				metadata.tags0 = row[6].(string)
+			}
+			if row[7] != "" {
+				metadata.tags1 = row[7].(string)
+			}
+			if row[8] != "" {
+				metadata.tags2 = row[8].(string)
+			}
+			if row[9] != "" {
+				metadata.tags3 = row[9].(string)
+			}
+			if row[10] != "" {
+				metadata.tags4 = row[10].(string)
+			}
+			return true, metadata
+		}
+	}
+	return false, metadata
+}
+
+func get_metadata(sheet_meta *sheets.ValueRange, sqlDB *sql.DB, tag string, date string) Metadata {
 	var metadata Metadata
 	row := sqlDB.QueryRow(`SELECT * FROM base_data WHERE tag =  ?`, tag)
 
 	err := row.Scan(&metadata.tag, &metadata.show_name, &metadata.show_nr, &metadata.dj_name, &metadata.picture, &metadata.description, &metadata.tags0, &metadata.tags1, &metadata.tags2, &metadata.tags3, &metadata.tags4)
 	if err != nil {
+		log.Fatal(err)
+	}
+
+	add, custom := check_custom_metadata(sheet_meta, metadata, date, tag)
+	if add {
+		return custom
 	}
 
 	return metadata
@@ -46,44 +92,44 @@ func update_show_nr(sqlDB *sql.DB, tag string) {
 }
 
 func update_meta_status(sqlDB *sql.DB, column string, tag string, date string) {
-  query := fmt.Sprintf(`UPDATE metadata SET %s = TRUE WHERE tag = ? AND date = ?`, column)
-  _, err := sqlDB.Exec(query, tag, date)
+	query := fmt.Sprintf(`UPDATE metadata SET %s = TRUE WHERE tag = ? AND date = ?`, column)
+	_, err := sqlDB.Exec(query, tag, date)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func get_meta_status(sqlDB *sql.DB, column string, tag string, date string) (bool) {
-  var status bool
-  query := fmt.Sprintf(`SELECT %s FROM metadata WHERE tag = ? AND date = ?`, column)
-  row := sqlDB.QueryRow(query, tag, date)
-  err:= row.Scan(&status)
+func get_meta_status(sqlDB *sql.DB, column string, tag string, date string) bool {
+	var status bool
+	query := fmt.Sprintf(`SELECT %s FROM metadata WHERE tag = ? AND date = ?`, column)
+	row := sqlDB.QueryRow(query, tag, date)
+	err := row.Scan(&status)
 	if err != nil {
 		log.Fatal(err)
 	}
-  if !status {
-    return true
-  }
-  return false
+	if !status {
+		return true
+	}
+	return false
 }
 
-func ready_for_upload(sqlDB *sql.DB, tag string, date string) (bool) {
-  var total int
+func ready_for_upload(sqlDB *sql.DB, tag string, date string) bool {
+	var total int
 	query := `
 		SELECT (SUM(mixcloud) + SUM(radiocult) + SUM(drive)) AS total_sum
 		FROM metadata
 		WHERE tag = ? AND date = ?`
-  row := sqlDB.QueryRow(query, tag, date)
-  err:= row.Scan(&total)
+	row := sqlDB.QueryRow(query, tag, date)
+	err := row.Scan(&total)
 	if err != nil {
 		log.Fatal(err)
-    return false 
+		return false
 	}
-  if total >= 3 {
-    return true
-  } else {
-    return false
-  }
+	if total >= 3 {
+		return true
+	} else {
+		return false
+	}
 }
 
 func check_row(sqlDB *sql.DB, tag string, date string) (bool, error) {
@@ -100,53 +146,52 @@ func check_row(sqlDB *sql.DB, tag string, date string) (bool, error) {
 	return true, nil
 }
 
-
 func new_meta_row(sqlDB *sql.DB, date string, metadata Metadata) {
-  var max_nr sql.NullInt64
-  var show_nr int
+	var max_nr sql.NullInt64
+	var show_nr int
 
 	row := sqlDB.QueryRow(`SELECT MAX(show_nr) FROM metadata WHERE tag =  ?`, metadata.tag)
-  err := row.Scan(&max_nr)
+	err := row.Scan(&max_nr)
 	if err != nil {
 		log.Fatal(err)
 	}
-  if max_nr.Valid {
-    show_nr = int(max_nr.Int64 + 1)
-  } else {
-    show_nr = 1
-  }
-  
-  exists, err := check_row(sqlDB, metadata.tag, date)
-  if !exists {
-    stmt, err := sqlDB.Prepare(`
+	if max_nr.Valid {
+		show_nr = int(max_nr.Int64 + 1)
+	} else {
+		show_nr = 1
+	}
+
+	exists, err := check_row(sqlDB, metadata.tag, date)
+	if !exists {
+		stmt, err := sqlDB.Prepare(`
       INSERT INTO metadata
       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     `)
-    if err != nil {
-      log.Fatal(err)
-    }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-    _, err = stmt.Exec(
-      date,
-      metadata.tag,
-      metadata.show_name,
-      show_nr,
-      metadata.dj_name,
-      metadata.picture,
-      metadata.description,
-      metadata.tags0,
-      metadata.tags1,
-      metadata.tags2,
-      metadata.tags3,
-      metadata.tags4,
-      0,
-      1, // because we havent implemented radiocult yet
-      0,
-    )
-    if err != nil {
-        log.Fatal(err)
-    }
-  } else {
-    log.Println("Row already exists")
-  }
+		_, err = stmt.Exec(
+			date,
+			metadata.tag,
+			metadata.show_name,
+			show_nr,
+			metadata.dj_name,
+			metadata.picture,
+			metadata.description,
+			metadata.tags0,
+			metadata.tags1,
+			metadata.tags2,
+			metadata.tags3,
+			metadata.tags4,
+			0,
+			1, // because we havent implemented radiocult yet
+			0,
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		log.Println("Row already exists")
+	}
 }
--- cmd/upload/upload.go
+++ cmd/upload/upload.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
-	"database/sql"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/mjoes/mixcloud-go/pkg/utils"
 	"google.golang.org/api/drive/v3"
 	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
 )
 
+var sheet_id string = "1XmJ8mXzMsBzDv13ZwM9tXasym5z3ZzlmNKC7xFudkzo"
 var drive_picture_folder string = "1t7JgNd4U1oQEYw4NTdHPUFAIxd9YJWq3"
 var archive_id string = "1qklZQWVpNRYJWLd0-0zBhxZLyWCHrtpe"
 
@@ -37,10 +39,10 @@ func main() {
 		return
 	}
 
-  // Open connection to metadata DB
+	// Open connection to metadata DB
 	sqlDB, _ := sql.Open("sqlite3", *db_path)
 	defer sqlDB.Close()
-  
+
 	fmt.Println("Starting upload of audio")
 
 	ctx := context.Background()
@@ -48,68 +50,79 @@ func main() {
 	if err != nil {
 		log.Fatal("Error:", err)
 	}
+	sheetsService, err := sheets.NewService(ctx, option.WithCredentialsFile(*cred_path))
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	sheet_meta, err := sheetsService.Spreadsheets.Values.Get(sheet_id, "meta").Do()
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
 	files, _ := os.ReadDir(audio_base_path)
 
 	log.Println("Looping through files in the to be uploaded folder")
 	for _, f := range files {
-    if string(f.Name()[0]) == "." {
-      continue
-    }
+		if string(f.Name()[0]) == "." {
+			continue
+		}
 		split_name := strings.Split(f.Name(), "_")
 		date := split_name[0]
 		tag := split_name[1]
-		metadata := get_metadata(sqlDB, tag)
-    new_meta_row(sqlDB, date, metadata)
+
+		metadata := get_metadata(sheet_meta, sqlDB, tag, date)
+		fmt.Println(metadata)
+		new_meta_row(sqlDB, date, metadata)
 
 		log.Println(f.Name(), "- Starting mixcloud upload process")
 		log.Println(f.Name(), "- Downloading picture to local storage")
 		picture_path := filepath.Join(picture_base_path, metadata.picture)
 		utils.GetPicture(metadata.picture, driveService, picture_path, drive_picture_folder)
-    audio_path := filepath.Join(audio_base_path, f.Name())
-    
-    // Mixcloud
+		audio_path := filepath.Join(audio_base_path, f.Name())
+
+		// Mixcloud
 		log.Println(f.Name(), "- Start upload to mixcloud")
-    if get_meta_status(sqlDB, "mixcloud", tag, date) {
-      // err = MixcloudUpload(audio_path, picture_path, metadata)
-      // if err != nil {
-      // 	log.Fatal("Error:", err)
-      // 	return
-      // }
-      update_meta_status(sqlDB, "mixcloud", tag, date)
-    } else {
-      log.Println("File already uploaded to Mixcloud")
-    }
-    
-    // Radiocult
+		if get_meta_status(sqlDB, "mixcloud", tag, date) {
+			// err = MixcloudUpload(audio_path, picture_path, metadata)
+			// if err != nil {
+			// 	log.Fatal("Error:", err)
+			// 	return
+			// }
+			update_meta_status(sqlDB, "mixcloud", tag, date)
+		} else {
+			log.Println("File already uploaded to Mixcloud")
+		}
+
+		// Radiocult
 		log.Println(f.Name(), "- Start upload to Radiocult")
 		log.Println("Yet to be implemented")
-    
-    // Google drive archive
+
+		// Google drive archive
 		log.Println(f.Name(), "- Start upload to Drive Archive")
-    if get_meta_status(sqlDB, "drive", tag, date) {
-      // err = utils.Upload(driveService, f.Name(), audio_path, archive_id)
-      // if err != nil {
-      //   log.Fatal(err)
-      //   retur
-      // }
-      update_meta_status(sqlDB, "drive", tag, date)
-    } else {
-      log.Println("File already uploaded to Drive")
-    }
-    
-    // Local drive archive
+		if get_meta_status(sqlDB, "drive", tag, date) {
+			// err = utils.Upload(driveService, f.Name(), audio_path, archive_id)
+			// if err != nil {
+			//   log.Fatal(err)
+			//   retur
+			// }
+			update_meta_status(sqlDB, "drive", tag, date)
+		} else {
+			log.Println("File already uploaded to Drive")
+		}
+
+		// Local drive archive
 		log.Println(f.Name(), "- Move to local archive")
-    if ready_for_upload(sqlDB, tag, date) {
-      err = utils.LocalMove(audio_path, filepath.Join(*archive_path, f.Name()))
-      if err != nil {
-        log.Fatal(err)
-        return
-      }
-      log.Println(f.Name(), "- Update show nr")
-      update_show_nr(sqlDB, tag)
-    } else {
-      log.Println("Not all upload stages complete, not moving to archive")
-    }
+		if ready_for_upload(sqlDB, tag, date) {
+			err = utils.LocalMove(audio_path, filepath.Join(*archive_path, f.Name()))
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+			log.Println(f.Name(), "- Update show nr")
+			update_show_nr(sqlDB, tag)
+		} else {
+			log.Println("Not all upload stages complete, not moving to archive")
+		}
 		log.Println("-----COMPLETED-----")
 	}
 	log.Println("Finished, check log for errors. Exiting program....")
